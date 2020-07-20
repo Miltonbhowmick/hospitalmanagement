23f1484ee4c7f4c2d8105e1cc7b10513bb8b472d
miltonbhowmick <miltonbhowmick6@gmail.com>
2020-07-20 21:02:51 +0600

    added search field in appointment admin

--- home/admin.py
+++ home/admin.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Category,Doctor,Appointment,Lab, Pharmacy,MedicineCompany, CategoryMedicine, FoodBlog, Contact
 
+class AppointmentAdmin(admin.ModelAdmin):
+	search_fields = ['email',]
+
 admin.site.register(Category)
 admin.site.register(Doctor)
-admin.site.register(Appointment)
+admin.site.register(Appointment,AppointmentAdmin)
 admin.site.register(Lab)
 admin.site.register(Pharmacy)
 admin.site.register(MedicineCompany)
